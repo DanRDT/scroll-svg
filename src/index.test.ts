243943d5a9ae0530bdfa796fa8241bf5f2b38327
unknown <pulberdan@gmail.com>
2023-03-01 18:12:54 -0500

    github workflows

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest"
+
+describe("Demo Test", () => {
+  it("SHould pass CI", () => {
+    expect(1).toBe(1)
+  })
+})
--- test/demo.test.js
+++
@@ -1,8 +0,0 @@
-const demo = require("../dist/index.js")
-
-describe("Demo Test", () => {
-  it("Test if it returns half of input", () => {
-    const num = demo.returnHalf(10)
-    expect(num).toBe(5)
-  })
-})
