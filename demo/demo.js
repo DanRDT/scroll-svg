5b70b7db7db91f3ed79fc299b7713e1a43003d19
unknown <pulberdan@gmail.com>
2023-03-02 16:50:13 -0500

    simple setup for 1 line

---
+++ demo/demo.js
@@ -0,0 +1,5 @@
+import scrollSvg from "../dist/index.mjs"
+
+const svg = document.querySelector("#scroll-line-2")
+
+scrollSvg(svg)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
-export function returnHalf(num: number): number {
-  return num / 2
+export default function scrollSvg(svgPath: SVGPathElement): void {
+  const sectionOne = document.querySelector("#section-1")
+  const sectionTwo = document.querySelector("#section-2")
+  const sectionThree = document.querySelector("#section-3")
+  const sectionFour = document.querySelector("#section-4")
+
+  // const svgPathLength = svgPath.getTotalLength() + ""
+
+  // svgPath.style.strokeDasharray = `${svgPathLength} ${svgPathLength}`
+  // svgPath.style.strokeDashoffset = svgPathLength
+
+  // const scrollLinePath: SVGPathElement | null = document.querySelector("#scroll-line-2")
+  const scrollLinePathLength = svgPath.getTotalLength() //
+
+  svgPath.style.strokeDasharray = scrollLinePathLength + " " + scrollLinePathLength
+  svgPath.style.strokeDashoffset = scrollLinePathLength + ""
+
+  const calcScrollLine = () => {
+    if (sectionOne === null || sectionTwo === null || sectionThree === null || sectionFour === null) {
+      console.error("section not found")
+      return
+    }
+
+    let currentScrollPosition = document.documentElement.scrollTop + document.body.scrollTop
+    let totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+
+    let sectionOneHeight = sectionOne.clientHeight
+    let sectionThreeHeight = sectionThree.clientHeight
+    let sectionFourHeight = sectionFour.clientHeight
+
+    const aboveHeight = sectionOneHeight
+    const belowHeight = sectionThreeHeight + sectionFourHeight
+
+    let adjustedTotalHeight = totalHeight - belowHeight
+    // let adjustedTotalHeight = totalHeight - sectionThreeHeight
+    let scrollPercentage = currentScrollPosition / adjustedTotalHeight
+
+    // wait for section2 to appear
+    let scrollPercentOffset = aboveHeight / 2.3 / adjustedTotalHeight
+
+    //calculate amount to draw
+    let offsetPercentage = scrollPercentage - scrollPercentOffset
+
+    if (offsetPercentage > 1) {
+      offsetPercentage = 1 // Max out SVG
+    }
+
+    let drawLength = scrollLinePathLength * offsetPercentage
+
+    // draw in reverse
+    svgPath.style.strokeDashoffset = scrollLinePathLength - drawLength + ""
+  }
+
+  window.addEventListener("scroll", calcScrollLine)
 }
