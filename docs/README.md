fc53fb6c8d30322b9c6fbaf4bb0bf22507678176
unknown <pulberdan@gmail.com>
2023-03-02 17:18:04 -0500

    barebones break down

--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,5 @@
 # scroll-svg
+
 A library to make animating svgs on scroll easier
+
+This library is still in the pre-alpha stage of development
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export default function scrollSvg(svgPath: SVGPathElement): void {
-  const sectionOne = document.querySelector("#section-1")
-  const sectionTwo = document.querySelector("#section-2")
-  const sectionThree = document.querySelector("#section-3")
-  const sectionFour = document.querySelector("#section-4")
+  // const sectionOne = document.querySelector("#section-1")
+  // const sectionTwo = document.querySelector("#section-2")
+  // const sectionThree = document.querySelector("#section-3")
+  // const sectionFour = document.querySelector("#section-4")
 
   // const svgPathLength = svgPath.getTotalLength() + ""
 
@@ -16,23 +16,24 @@ export default function scrollSvg(svgPath: SVGPathElement): void {
   svgPath.style.strokeDashoffset = scrollLinePathLength + ""
 
   const calcScrollLine = () => {
-    if (sectionOne === null || sectionTwo === null || sectionThree === null || sectionFour === null) {
-      console.error("section not found")
-      return
-    }
+    // if (sectionOne === null || sectionTwo === null || sectionThree === null || sectionFour === null) {
+    //   console.error("section not found")
+    //   return
+    // }
 
     let currentScrollPosition = document.documentElement.scrollTop + document.body.scrollTop
     let totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
 
-    let sectionOneHeight = sectionOne.clientHeight
-    let sectionThreeHeight = sectionThree.clientHeight
-    let sectionFourHeight = sectionFour.clientHeight
+    // let sectionOneHeight = sectionOne.clientHeight
+    // let sectionThreeHeight = sectionThree.clientHeight
+    // let sectionFourHeight = sectionFour.clientHeight
 
-    const aboveHeight = sectionOneHeight
-    const belowHeight = sectionThreeHeight + sectionFourHeight
+    // const aboveHeight = sectionOneHeight
+    const aboveHeight = calcHeightAbove(svgPath)
+    // const belowHeight = sectionThreeHeight + sectionFourHeight
+    const belowHeight = calcHeightBelow(svgPath)
 
     let adjustedTotalHeight = totalHeight - belowHeight
-    // let adjustedTotalHeight = totalHeight - sectionThreeHeight
     let scrollPercentage = currentScrollPosition / adjustedTotalHeight
 
     // wait for section2 to appear
@@ -52,4 +53,14 @@ export default function scrollSvg(svgPath: SVGPathElement): void {
   }
 
   window.addEventListener("scroll", calcScrollLine)
+}
+
+function calcHeightAbove(SVGPathElement: SVGPathElement): number {
+  // TODO: calculate height above
+  return 0
+}
+
+function calcHeightBelow(SVGPathElement: SVGPathElement): number {
+  // TODO: calculate height below
+  return 0
 }
