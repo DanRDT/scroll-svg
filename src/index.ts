7ba40422eabf92751a7af15764402f726f3c3ac6
unknown <pulberdan@gmail.com>
2023-03-04 01:04:22 -0500

    skeletol structure of calculating scroll position and drawlength

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,85 @@
 export default function scrollSvg(svgPath: SVGPathElement): void {
-  // const sectionOne = document.querySelector("#section-1")
-  // const sectionTwo = document.querySelector("#section-2")
-  // const sectionThree = document.querySelector("#section-3")
-  // const sectionFour = document.querySelector("#section-4")
-
-  // const svgPathLength = svgPath.getTotalLength() + ""
-
-  // svgPath.style.strokeDasharray = `${svgPathLength} ${svgPathLength}`
-  // svgPath.style.strokeDashoffset = svgPathLength
-
-  // const scrollLinePath: SVGPathElement | null = document.querySelector("#scroll-line-2")
-
+  // check if svgPath is a path
   if (svgPath === null) {
-    console.error("svgPath not found ~ check id or class")
-    return
+    return console.error("svgPath not found ~ check id or class")
   } else if (svgPath.tagName !== "path") {
-    console.error("svgPath is not a path")
-    return
+    return console.error("svgPath is not a path")
   } else if (svgPath.getTotalLength() === 0) {
-    console.error("svgPath has no length")
+    return console.error("svgPath has no length")
   }
 
-  const scrollLinePathLength = svgPath.getTotalLength() //
+  const svgPathLength = svgPath.getTotalLength()
+  svgPath.style.strokeDasharray = svgPathLength + " " + svgPathLength
+  const dashOffset = svgPath.style
 
-  svgPath.style.strokeDasharray = scrollLinePathLength + " " + scrollLinePathLength
-  svgPath.style.strokeDashoffset = scrollLinePathLength + ""
+  // svgPath.style.strokeDashoffset = svgPathLength + 1 + ""
+  dashOffset.strokeDashoffset = 0 + ""
 
   const calcScrollLine = () => {
-    let currentScrollPosition = document.documentElement.scrollTop
-    let totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    const percentToDraw = calcPercentToDraw(svgPath)
+    const pixelOffset = percentToPixelOffset(percentToDraw, svgPath)
+    dashOffset.strokeDashoffset = pixelOffset + ""
 
-    const aboveHeight = calcHeightAbove(svgPath)
-    const belowHeight = calcHeightBelow(svgPath)
-
-    let adjustedTotalHeight = totalHeight - belowHeight
-    let scrollPercentage = currentScrollPosition / adjustedTotalHeight
-
-    // wait for section2 to appear
-    let scrollPercentOffset = aboveHeight / 2 / adjustedTotalHeight
-
-    //calculate amount to draw
-    let offsetPercentage = scrollPercentage - scrollPercentOffset
-
-    if (offsetPercentage > 1) {
-      offsetPercentage = 1 // Max out SVG
-    }
-
-    let drawLength = scrollLinePathLength * offsetPercentage
-
-    // draw in reverse
-    svgPath.style.strokeDashoffset = scrollLinePathLength - drawLength + ""
+    // let currentScrollPosition = document.documentElement.scrollTop
+    // let totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    // const aboveHeight = calcHeightAbove(svgPath)
+    // const belowHeight = calcHeightBelow(svgPath)
+    // console.log(
+    //   `Top: ${Math.round(svgPath.getBoundingClientRect().top)} Bottom: ${Math.round(
+    //     svgPath.getBoundingClientRect().bottom
+    //   )}`
+    // )
+    // find how far down the path you are using
+    // let adjustedTotalHeight = totalHeight - belowHeight
+    // let scrollPercentage = currentScrollPosition / adjustedTotalHeight
+    // // wait for section2 to appear
+    // let scrollPercentOffset = aboveHeight / 2 / adjustedTotalHeight
+    // //calculate amount to draw
+    // let offsetPercentage = scrollPercentage - scrollPercentOffset
+    // if (offsetPercentage > 1) {
+    //   offsetPercentage = 1 // Max out SVG
+    // }
+    // let drawLength = svgPathLength * offsetPercentage
+    // // draw in reverse
+    // svgPath.style.strokeDashoffset = svgPathLength - drawLength + ""
   }
 
   window.addEventListener("scroll", calcScrollLine)
 }
 
-// height = element height
-// width = element width
-// left and right are distance from left and right of viewport
-// x = left
-// y = top
+// function calcHeightAbove(SVGPathElement: SVGPathElement): number {
+//   const heightAbove = SVGPathElement.getBoundingClientRect().top + document.documentElement.scrollTop
+//   return Math.round(heightAbove)
+// }
 
-function calcHeightAbove(SVGPathElement: SVGPathElement): number {
-  return Math.round(SVGPathElement.getBoundingClientRect().top + document.documentElement.scrollTop)
+// function calcHeightBelow(SVGPathElement: SVGPathElement): number {
+//   const totalHeight = document.documentElement.scrollHeight
+//   const heightToBottomOfElement =
+//     SVGPathElement.getBoundingClientRect().bottom + document.documentElement.scrollTop
+//   const heightBelow = totalHeight - heightToBottomOfElement
+
+//   return Math.round(heightBelow)
+// }
+
+// gets the percent as a decimal that the svg should be drawn and converts it to a pixel offset
+function percentToPixelOffset(percent: number, svgPath: SVGPathElement): number {
+  const svgPathLength = svgPath.getTotalLength()
+  return percent * svgPathLength
 }
 
-function calcHeightBelow(SVGPathElement: SVGPathElement): number {
-  const totalHeight = document.documentElement.scrollHeight
-  const heightToBottomOfElement =
-    SVGPathElement.getBoundingClientRect().bottom + document.documentElement.scrollTop
-  const heightBelow = totalHeight - heightToBottomOfElement
+function calcPercentToDraw(svgPath: SVGPathElement): number {
+  //TODO calculate the percentage of the svg to draw
 
-  return Math.round(heightBelow)
+  let offsetPercentage = 0.9
+
+  // Max out SVG
+  if (offsetPercentage > 1) {
+    offsetPercentage = 1
+  } else if (offsetPercentage < 0) {
+    offsetPercentage = 0
+  }
+
+  // flip the percentage
+  offsetPercentage = 1 - offsetPercentage
+  return offsetPercentage
 }
