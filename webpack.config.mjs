f3594f39fc865841d29e3ee5b4a417ce48a1bad2
Dan <pulberdan@gmail.com>
2026-02-01 19:42:07 -0500

    build: change webpack __dirname

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,8 +1,6 @@
 import path from 'path'
-import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = import.meta.dirname
 
 export default {
   entry: './dist/index.mjs',
